5e487d2a7b6084c1617f7190098d3c5dc65fe5dc
nick.huang <nick.huang@wegames.com.tw>
2020-04-12 21:31:50 +0800

    feat(DB): success connction database

--- src/main/java/book/BookServerlet.java
+++ src/main/java/book/BookServerlet.java
@@ -15,6 +15,8 @@ import java.io.PrintWriter;
 import book.*;
 import java.util.*;
 import com.google.gson.*;
+import java.util.Properties;
+
 
 
 @WebServlet(name = "BookServerlet", urlPatterns = "/BookServerlet")
@@ -26,54 +28,44 @@ public class BookServerlet  extends HttpServlet {
         PreparedStatement ps = null;
         ResultSet rs = null;
 
+
+
         try {
-            List<User> userList = new ArrayList<>();
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/books","root","123456789");
+            Class.forName("com.mysql.jdbc.Driver");// include this line in your code.
+
+            conn =  DriverManager.getConnection("jdbc:mysql://localhost/bookstore?" +
+                    "user=root&password=123456789");
             ps = conn.prepareStatement("select * from book");
             rs = ps.executeQuery();
-//
-//            Cookie c1 = new Cookie("arkadas", "Ozgur");
-//            Cookie c2 = new Cookie("arkadas2", "Emre");
-// Burada cookie lerimizi tanımladık. Name ve değerini girdik.
 
-//            response.addCookie(c1);
-//            response.addCookie(c2);
-            // response ile cookie lerimizi ekledik.
+            while(rs.next()){
+                int book_id = rs.getInt("book_id");
+                String title = rs.getString("title");
+                String author = rs.getString("author");
+                float price =  rs.getFloat("price");
+                System.out.format("%s, %s, %s, %s \n", book_id, title, author,price);
 
-//            response.addHeader("Set-Cookie","arkadas3-Umit Arslan");// addheader ile de cookie ekleyebiliyoruz.
-//            Cookie[] cookie=request.getCookies();
-//            if (cookie!=null)
-//            {
-//                for (Cookie c:cookie)
-//                {
-//                    System.out.println("Cookie adı"+ " "+ c.getName()+  " "+ "Cookie değeri"+ " " +c.getValue());
-//                    //Bütün cookie leri name ve değerini konsola yazdırdık.
-//                    PrintWriter out = response.getWriter();
-//                    out.println("<!DOCTYPE html>");
-//                    out.println("<html>");
-//                    out.println("<head>");
-//                    out.println("<title>Hello! World!</title>");
-//                    out.println("</head>");
-//                    out.println("<body>");
-//                    out.println("<h1>Hello! World!</h1>");
-//                    out.println("</body>");
-//                    out.println("</html>");
-//
-//                }
-//            }
-            response.setContentType("application/json");
-            Person p = new Person();
-            p.setAge(10);
-            p.setEmail("bob@hotmail.com");
-            p.setName("Bob Qin");
-            p.setSex(Person.SEX_MAN);
-            List<String> telephones = new ArrayList<String>();
-            telephones.add("13813841385");
-            telephones.add("021-454566778");
-            p.setTelephones(telephones);
-            PrintWriter writer = response.getWriter();
-            writer.write(new Gson().toJson(p, Person.class));
-            writer.flush();
+            }
+
+
+//            Properties info = new Properties();
+//            info.put("user", "root");
+//            info.put("password", "test");
+
+
+//            response.setContentType("application/json");
+//            Person p = new Person();
+//            p.setAge(10);
+//            p.setEmail("bob@hotmail.com");
+//            p.setName("Bob Qin");
+//            p.setSex(Person.SEX_MAN);
+//            List<String> telephones = new ArrayList<String>();
+//            telephones.add("13813841385");
+//            telephones.add("021-454566778");
+//            p.setTelephones(telephones);
+//            PrintWriter writer = response.getWriter();
+//            writer.write(new Gson().toJson(p, Person.class));
+//            writer.flush();
 
         }
         catch (Exception e)
